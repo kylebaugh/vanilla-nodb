654c2441fdc05c025e9e28a2cbb4d5fc8af32e6f
kylebaugh <kylebaugh1@gmail.com>
2024-06-27 14:41:15 -0600

    get and post endpoints working

--- server/server.js
+++ server/server.js
@@ -2,6 +2,29 @@
 import express from 'express'
 import cors from 'cors'
 
+const mockData = [
+    {
+        "id": 1,
+        "name": "Vanilla Coke",
+        "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXnNmIsz7RSLJIqBatdRosuERkyzKy0e-Kg&usqp=CAU",
+        "popularity": 0
+    },
+    {
+        "id": 2,
+        "name": "Dr. Pepper",
+        "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrNOB8XuhOZRYsR9ed2T9StxnUSlXSawSssQ&usqp=CAU",
+        "popularity": 0
+    },
+    {
+        "id": 3,
+        "name": "Sparkling Water",
+        "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEXD1BCrfd5zPPHRnv3NjQ0whUwxubKGfqzA&usqp=CAU",
+        "popularity": 0
+    }
+]
+
+let globalId = 4
+
 // Setup express instance
 const app = express()
 
@@ -11,6 +34,25 @@ app.use(cors())
 app.use(express.static('src'))
 
 // ENDPOINTS GO HERE
+app.get('/drinks', (request, response) => {
+    response.send(mockData)
+})
+
+app.post('/newDrink', (request, response) => {
+
+    const newDrink = {
+        id: globalId,
+        name: request.body.name,
+        imageUrl: request.body.imageUrl,
+        popularity: 0
+    }
+
+    mockData.push(newDrink)
+
+    globalId++
+
+    response.send(mockData)
+})
 
 
 // Open server using app.listen
